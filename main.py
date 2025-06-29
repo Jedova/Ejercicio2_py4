f2c54597a80d8b938a769415c36b0e9cab4fffc6
Jedova <nemergy.consulting@gmail.com>
2025-06-29 22:42:39 +0000

    avance para el cierre

--- main.py
+++ main.py
@@ -27,6 +27,27 @@ print(f"\nCorreo: {compra.correo}")
 print(f"Tarjeta: {compra.tarjeta}")
 print(f"Tipo de membresía: {compra.__class__.__name__}")
 
+##Ahora si buscamos ajustar membresía##
+
+cambio = input(f"¿deseas cambiar de tu membresía {compra.__class__.__name__}? SI/NO: ").strip().upper()
+
+if cambio == "SI":
+    try:
+        
+        tipo2 = int(input("Elige tipo de membresía, la cual debe ser distinta a la actual (0: Gratis; 1: Básico; 2:Familiar; 3: Sin Conexión; 4: Pro): "))
+        
+        ##Validar el tipo
+        if tipo2 not in [0,1,2,3,4]:
+            print("Tipo de membresía no válido. No se realiza el cambio.")
+        elif tipo2 == tipo:
+            print (f"No puedes cambiar a la misma membresía. Se mantiene:  {compra.__class__.__name__} ")
+        else:
+            compra = crear_membresia(compra.correo, compra.tarjeta, tipo2)
+            print(f"Tu nueva membresía es: {compra.__class__.__name__}")
+    except ValueError:
+        print("Entrada inválida. Debes ingresar un número entre 0 y 4.")
+else:
+    print(f"Tu membresía sigue siendo: {compra.__class__.__name__}")
 
 
 
